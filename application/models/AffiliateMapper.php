bb887b65cc19c57aa77fdc69043cf1bdefd75dd7
Nully <nully.nl@gmail.com>
2010-08-27 18:33:52 +0900

    add mode files

---
+++ application/models/AffiliateMapper.php
@@ -0,0 +1,68 @@
+<?php
+class Jobeet_Model_AffiliateMapper extends Jobeet_Model_MapperAbstract
+{
+    /**
+     * get table data gateway class
+     *
+     * @Override  getDbTable
+     * @return Zend_Db_Table_Abstract
+     */
+    public function getDbTable()
+    {
+        if(!$this->_dbTable) {
+            $this->setDbTable("Jobeet_Model_DbTable_Affiliate");
+        }
+
+        return $this->_dbTable;
+    }
+
+
+    /**
+     * save a affiliate data
+     *
+     * @access public
+     */
+    public function save(Jobeet_Model_Affiliate $affiliate)
+    {
+        // @TODO: save action
+    }
+
+
+    /**
+     * find one affiliate data
+     *
+     * @access public
+     * @param  
+     */
+    public function find($id, Jobeet_Model_Affiliate $affiliate)
+    {
+        $result = $this->getDbTable()->find($id);
+
+        if(count($result) == 0) {
+            return;
+        }
+
+        $row = $result->current();
+        $affiliate->setOptions($row->toArray());
+    }
+
+
+    /**
+     * fetch all affiliates
+     *
+     * @access public
+     * @return Array
+     */
+    public function fetchAll()
+    {
+        $results = $this->getDbTable()->fetchAll();
+        $affiliates = array();
+        foreach($results as $row) {
+            $affiliate = new Jobeet_Model_Affiliate();
+            $affiliate->setOptions($row->toArray());
+            $affiliates[] = $affiliate;
+        }
+
+        return $affiliates;
+    }
+}

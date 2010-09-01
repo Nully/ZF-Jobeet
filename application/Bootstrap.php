a56276c7cf13c7b305a64d56eaab06fcc3e317ba
nully <nully.nl@gmail.com>
2010-09-02 00:09:03 +0900

    add initViewHelpers method

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,20 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    /**
+     * ViewHelper の初期定義を行います
+     * 
+     */
+    protected function _initViewHelpers()
+    {
+        $view = $this->bootstrap("view")->getResource("view");
+        $view->headLink(array(
+            "rel"   => "stylesheet",
+            "href"  => "/css/import.css",
+            "type"  => "text/css",
+            "media" => "Screen,Projection,TV",
+        ));
+    }
 
 }
 
